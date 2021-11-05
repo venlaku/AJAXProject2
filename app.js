56a052acd61bb50635b827bc30f6810d601ab5e7
VenlaKu <venla.i.kuosmanen@gmail.com>
2021-11-05 09:30:36 +0200

    changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var url="";
+//creating request
+var url="http://www.last.fm/api/auth/?api_key=1dc459d3b860f7223e792e3d93287dad";
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.open("Get", url, true);
 xmlhttp.send();
