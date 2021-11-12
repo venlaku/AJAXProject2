ccaad43a4e49e17b08ad43b7c4c1d8ca58522945
VenlaKu <venla.i.kuosmanen@gmail.com>
2021-11-12 09:51:13 +0200

    apikey

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var api_key = "1dc459d3b860f7223e792e3d93287dad";
+var apiKey = "1dc459d3b860f7223e792e3d93287dad";
 
 var getArtist = new XMLHttpRequest();
 var getAlbum = new XMLHttpRequest();
@@ -7,65 +7,65 @@ function showResult () {
 
     if (getArtist.readyState == 4)
 	{
-		var json = JSON.parse(request1.responseText);
+		var json = JSON.parse(getArtist.responseText);
         var str = JSON.stringify(json,undefined,2);
 		
-		var result1_1 = "<br/>"+"<strong>Artist Name:</strong> "+ json.artist.name+"<br/><br/>";
+		var result1_1 = "Artist Name: "+ json.artist.name;
 		
 		var src = json.artist.image[3];
 		var img = src["#text"];
 		
-		var result1_2 = "<center><img width= '100' height='100' title='image' src='"+img+"'/></center>"+"<br/><br/>";
+		var result1_2 = "<img title='image' src='"+img+"'/>";
 		
-		var result1_3 = "<strong>Biography:</strong><br/>"+json.artist.bio.summary+"<br/><br/><strong>Debut Place:</strong> "
-		+json.artist.bio.placeformed+"<br/><br/><strong>Debut Year: </strong>"+json.artist.bio.yearformed+"<br/>";
+		var result1_3 = "Biography:"+json.artist.bio.summary+"Debut Place: "
+		+json.artist.bio.placeformed+"Debut Year: "+json.artist.bio.yearformed;
 		
-        document.getElementById("output1").innerHTML = "<ul>"+result1_1+"</ul>"+"<ul>"+result1_2+"</ul>"+"<ul>"+result1_3+"</ul>";
+        document.getElementById("artist-info").innerHTML = "<ul>"+result1_1+"</ul>"+"<ul>"+result1_2+"</ul>"+"<ul>"+result1_3+"</ul>";
 	}
 	
 	if (getAlbum.readyState == 4)
 	
 	{
-        var json = JSON.parse(request2.responseText);
+        var json = JSON.parse(getAlbum.responseText);
         var str = JSON.stringify(json,undefined,2);
 		
-		var albSrc;
-		var albImg;
+		var albumSource;
+		var albumImage;
 		var albumName = new Array();
 		var i=0;
 		while (i<10)
 		{
 					
-			albSrc =  json.topalbums.album[i].image[2];
-			albImg = albSrc["#text"];
+			albumSource =  json.topalbums.album[i].image[2];
+			albImg = albumSource["#text"];
 			
-			albumName[i] = "<li>"+json.topalbums.album[i].name+"<br/>"
-							+"<a href='"+json.topalbums.album[i].url+"'>Album info</a>"+"<br/>"
-							+"<img width= '100' height='100' title='image' src='"+albImg+"'/>"+"<br/><br/></li>";
-			
+			albumName[i] = "<li>"+json.topalbums.album[i].name
+							+"<a href='"+json.topalbums.album[i].url+"'>Album info</a>"
+							+"<img title='image' src='"+albumImage+"'/>"+"</li>";			
 							i++;
 		}
 		
-		document.getElementById("output2").innerHTML = "<ul>"+albumName+"</ul>";		
+		document.getElementById("album").innerHTML = "<ul>"+albumName+"</ul>";		
     }
 	
 }
 
 
 function sendRequest() {
-	
-	var method1 = "artist.info";
+
+	var host ='https://ws.audioscrobbler.com/2.0/?method=';
+	var methodartists = "artist.getinfo";
 	getArtist.onreadystatechange = showResult;
 	var artist = document.getElementById("searchbar").value;
-    getArtist.open("GET","",true);
+    getArtist.open("GET",host+methodartists+"&artist="+artist+ "&api_key="+ apiKey + "&format=json",true);
     getArtist.withCredentials = "true";
     getArtist.send(null);
 	
 
-	var method2 = "artist.getTopAlbums";
+	var methodalbums = "artist.getAlbums";
 	getAlbum.onreadystatechange = showResult;
 	var artist = document.getElementById("searchbar").value;
-	getAlbum.open("GET","",true);
+	getAlbum.open("GET",host+methodalbums+"&artist="+artist+ "&api_key="+ apiKey + "&format=json",true);
 	getAlbum.withCredentials = "true";
 	getAlbum.send(null);
 	
