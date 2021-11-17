c8ef852b1908ba87b4f21baae0afbb38daf025b6
VenlaKu <venla.i.kuosmanen@gmail.com>
2021-11-17 19:41:23 +0200

    top50 artist list

--- app.js
+++ app.js
@@ -6,69 +6,69 @@ var secret ="163a2390a29cdf9a123cbda2ca4eef21";
 //xmlhttprequests for getting album and artist
 var getArtist = new XMLHttpRequest();
 var getAlbum = new XMLHttpRequest();
-var searchButton=document.getElementById("searchbar");
-
-
-//add by pressing enter. Does the same as clicking Btn
-searchButton.addEventListener("keyup", function(e) {
-    if (e.code === 'Enter') {
-        document.getElementById("btn").click();
-    }
-});
+var getTopArtists = new XMLHttpRequest();
+var searchBar=document.getElementById("searchbar");
 
 //function for artist and album results
 function showResult () {
 
-    if (getArtist.readyState == 4)
+    if (getArtist.readyState == 4 && getArtist.status==200)
 	{
 		var json = JSON.parse(getArtist.responseText);
 		//getting artist 
-		var resultName = "Artist Name: "+ json.artist.name;
-		//artist bibliography
-		var resultBibliography = "Biography:"+json.artist.bio.summary+"Debut Place: "
-		+json.artist.bio.placeformed+"Debut Year: "+json.artist.bio.yearformed;
+		var resultName = "Artist Name:"+json.artist.name;
+		var resultBibliography = "Biography:"+json.artist.bio.summary;
 		//results to artist-info as a list
-        document.getElementById("artist-info").innerHTML = "<ul>"+resultName+"</ul>"+"<ul>"+"<ul>"+resultBibliography+"</ul>";
+        document.getElementById("artist-info").innerHTML = "<ul>"+resultName+"</ul>"+"<ul>"+resultBibliography+"</ul>";
 	}
 	//albums of the artist
-	if (getArtist.readyState == 4) {
-        var json = JSON.parse(getArtist.responseText);
+	if (getAlbum.readyState == 4 && getAlbum.status==200) {
+        var json = JSON.parse(getAlbum.responseText);
 		//album variant
 		var albumName = new Array();
-		var i=2;
-		//getting top 10 albums of the artist. Goes through loop until 1 is not less than 10
-		while (i<10)
+		var i=0;
+		//getting top 10 albums of the artist. Goes through loop until i is not less than 10
+		for (var i=0; i < json.topalbums.album.length; i++)
 		{					
-			albumName[i] = "<li>"+json.topalbums+"</li>";			
-			i++;
+			albumName[i] = `<li><a href="${json.topalbums.album[i].url}">${json.topalbums.album[i].name}</a></li>`;			
 		}	
-		document.getElementById("album").innerHTML = "<ul>"+albumName+"</ul>";		
+		document.getElementById("album").innerHTML = albumName.join('');		
     }
 }
 
-function artistList(){
-	var json = JSON.parse(getArtist.responseText);
-	var artistsNames= json.artist.name;
-	//results to artists as a list
-	document.getElementById("artists").innerHTML = "<li>"+artistsNames+"</li>";
-	var methodartistsurl = "artist.getinfo";
-	getArtist.onreadystatechange = showResult;
-	getArtist.open("GET",host+"?method="+methodartistsurl+"&artist="+artist+"&api_key="+apiKey+"&format=json",true);
-	getArtist.send();
+function getAllArtists() {
+	if (getTopArtists.readyState == 4 && getTopArtists.status==200) {
+		var json = JSON.parse(getTopArtists.responseText);
+		var artistsNames= new Array();
+		// //results to artists as a list
+		 for (var i=0; i < json.artists.artist.length; i++) {
+			artistsNames[i] = `<li onclick="searchArtist('${json.artists.artist[i].name}')">${json.artists.artist[i].name}</li>`;			
+		}
+		console.log(artistsNames, json);
+		document.getElementById("artists").innerHTML = artistsNames.join('');
+	}
 }
 
-function sendRequest() {
+var methodartistsurl = "chart.gettopartists";
+getTopArtists.onreadystatechange = getAllArtists;
+getTopArtists.open("GET",host+"?method="+methodartistsurl+"&api_key="+apiKey+"&format=json",true);
+getTopArtists.send();
 
+function sendRequest(event) {
+	event.preventDefault();
+	var artist = document.getElementById("searchbar").value;
+	searchArtist(artist);
+	return false;		
+}
+
+function searchArtist(artist) {
 	var methodartist = "artist.getinfo";
 	getArtist.onreadystatechange = showResult;
-	var artist = document.getElementById("searchbar").value;
     getArtist.open("GET",host+"?method="+methodartist+"&artist="+artist+ "&api_key="+ apiKey + "&format=json",true);
     getArtist.send();
 	
 	var methodalbums = "artist.gettopalbums";
-	getArtist.onreadystatechange = showResult;
-	var artist = document.getElementById("searchbar").value;
-	var album = '';
-	getArtist.open("GET",host+"?method="+methodalbums+"&artist="+artist+"&album="+album+"&api_key="+apiKey+"&format=json",true);
-	getArtist.send();	
+	getAlbum.onreadystatechange = showResult;
+	getAlbum.open("GET",host+"?method="+methodalbums+"&artist="+artist+"&api_key="+apiKey+"&format=json"+"&limit=10",true);
+	getAlbum.send();
 }
